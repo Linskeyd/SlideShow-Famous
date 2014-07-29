6d268f17e4bf242a6615a61cac7571525236ab9b
David Linskey <linskeyd@gmail.com>
2014-07-29 09:56:08 -0400

    Initial commit starting the slideshow example project

---
+++ src/views/EmptyView.js
@@ -0,0 +1,13 @@
+/*** EmptyView.js ***/
+
+// define this module in Require.js
+define(function(require, exports, module) {
+
+	// import additional modules to be used in this view
+	var View = require('famous/core/View');
+	var Surface = require('famous/core/Surface');
+	var Transform = require('famous/core/Transform');
+	var StateModifier = require('famous/modifiers/StateModifier');
+
+	
+});
