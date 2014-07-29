15e6764fcdba3a291b68b58863b6dcd24714b81c
David Linskey <linskeyd@gmail.com>
2014-07-29 10:05:09 -0400

    Finished initial implementation of EmptyView.js

--- src/views/EmptyView.js
+++ src/views/EmptyView.js
@@ -9,5 +9,21 @@ define(function(require, exports, module) {
 	var Transform = require('famous/core/Transform');
 	var StateModifier = require('famous/modifiers/StateModifier');
 
-	
-});
+	// Constructor function for the EmptyView class
+	function EmptyView() {
+
+		// Applies View's constructor function to EmptyView
+		View.apply(this, arguments);
+
+	}
+
+	// Establishes prototype chain for EmptyView class to inherit from View
+	EmptyView.prototype = Object.create(View.prototype);
+	EmptyView.prototype.constructor = EmptyView;
+
+	// Default options for the EmptyView class
+	EmptyView.DEFAULT_OPTIONS = {};
+
+	// Define your helper functions and prototype methods here
+
+	module.exports = EmptyView;
